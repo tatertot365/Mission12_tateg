ecbe3ee28e3d04151aecb208d5f071c32906d9a3
tatertot365 <tate.gillespie@gmail.com>
2023-03-22 15:17:18 -0600

    update

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,7 @@ import React, { Component } from "react";
 class Header extends Component {
     render() {
         return (
-            <header style={{color: "red"}}>
+            <header style={{color: "blue"}}>
             <h1>Hello this is a React Website about March Madness!</h1>
             <h2>This website shows school information for all schools in March Maddness this year!</h2>
             </header>
